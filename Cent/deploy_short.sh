8e2605528df08c041fb8861e8d803a225965f3f9
lengsophy <sophyleng071@gmail.com>
2016-05-26 10:02:15 +0700

    Update deploy_short.sh

--- Cent/deploy_short.sh
+++ Cent/deploy_short.sh
@@ -4,6 +4,7 @@ sudo tar -zxvf bundle.tgz
 wait
 sudo yum install nodejs npm
 wait
+#npm install pm2 -g
 cd bundle/programs/server/
 sudo npm install
 wait
@@ -13,5 +14,7 @@ cd ../../..
 sudo chmod -R 777 bundle
 sudo chown -R apache:apache bundle
 wait
-PORT=3000 MONGO_URL=mongodb://localhost:27017/meteor ROOT_URL=$1  nohup node bundle/main.js &
+#PORT=80 MONGO_URL=mongodb://localhost:27017/meteor ROOT_URL=$1 pm2 start --name "1000" main.js
+PORT=80 MONGO_URL=mongodb://localhost:27017/meteor ROOT_URL=$1  nohup node bundle/main.js &
+
 
